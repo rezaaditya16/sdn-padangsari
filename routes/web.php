cc365535877b99830e69aed5b273473913dce0cf
Bilunzzz <hainakamakmur06@gmail.com>
2025-03-18 23:51:28 +0700

    Add Livewire routes for About, Achievement, Announcement, Contact, Gallery, Home, News, and Profile pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\AboutPage;
+use App\Http\Livewire\AchievementPage;
+use App\Http\Livewire\AnnouncementPage;
+use App\Http\Livewire\ContactPage;
+use App\Http\Livewire\GalleryPage;
+use App\Http\Livewire\HomePage;
+use App\Http\Livewire\NewsPage;
+use App\Http\Livewire\ProfilePage;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/about', AboutPage::class);
+Route::get('/achievement', AchievementPage::class);
+Route::get('/announcement', AnnouncementPage::class);
+Route::get('/contact', ContactPage::class);
+Route::get('/gallery', GalleryPage::class);
+Route::get('/home', HomePage::class);
+Route::get('/news', NewsPage::class);
+Route::get('/profile', ProfilePage::class);
+
