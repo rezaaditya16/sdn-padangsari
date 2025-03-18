dd42fe2a69285c8dc2dadb4163e0b039c85b342e
Bilunzzz <hainakamakmur06@gmail.com>
2025-03-19 00:05:37 +0700

    Refactor HomePage component and update routes for improved Livewire integration

--- app/Livewire/Livewire/HomePage.php
+++ app/Livewire/Livewire/HomePage.php
@@ -1,13 +1,21 @@
 <?php
 
-namespace App\Livewire\Livewire;
+namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Post;
 
 class HomePage extends Component
 {
+    public $latestPosts;
+
+    public function mount()
+    {
+        $this->latestPosts = Post::latest()->take(3)->get();
+    }
+
     public function render()
     {
-        return view('livewire.livewire.home-page');
+        return view('livewire.home-page');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Livewire\HomePage;
 use App\Http\Livewire\AboutPage;
 use App\Http\Livewire\AchievementPage;
 use App\Http\Livewire\AnnouncementPage;
 use App\Http\Livewire\ContactPage;
 use App\Http\Livewire\GalleryPage;
-use App\Http\Livewire\HomePage;
 use App\Http\Livewire\NewsPage;
 use App\Http\Livewire\ProfilePage;
 
-Route::get('/about', function () {
-    return Livewire::mount(AboutPage::class);
-});
-
-Route::get('/achievement', function () {
-    return Livewire::mount(AchievementPage::class);
-});
-
-Route::get('/announcement', function () {
-    return Livewire::mount(AnnouncementPage::class);
-});
-
-Route::get('/contact', function () {
-    return Livewire::mount(ContactPage::class);
-});
-
-Route::get('/gallery', function () {
-    return Livewire::mount(GalleryPage::class);
-});
-
-Route::get('/home', function () {
-    return Livewire::mount(HomePage::class);
-});
-
-Route::get('/news', function () {
-    return Livewire::mount(NewsPage::class);
-});
-
-Route::get('/profile', function () {
-    return Livewire::mount(ProfilePage::class);
-});
-
+Route::get('/', HomePage::class)->name('home');
+Route::get('/about', AboutPage::class)->name('about');
+Route::get('/achievement', AchievementPage::class)->name('achievement');
+Route::get('/announcement', AnnouncementPage::class)->name('announcement');
+Route::get('/contact', ContactPage::class)->name('contact');
+Route::get('/gallery', GalleryPage::class)->name('gallery');
+Route::get('/news', NewsPage::class)->name('news');
+Route::get('/profile', ProfilePage::class)->name('profile');
