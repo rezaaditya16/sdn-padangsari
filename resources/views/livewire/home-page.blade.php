6914a93ec61f11787613c84606ddd47e7d5332eb
Aufadholi <aufa.fadholi@gmail.com>
2025-03-28 22:32:35 +0700

    Updated kontak page structure for better spacing and styling

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,4 +1,27 @@
-<div>
-    <h1 class="flex pt-20">Welcome to the Home Page</h1>
-    <p>Selamat datang di SDN Padangsari.</p>
-</div>
+<div class="bg-white min-h-screen flex pt-10 flex-col items-center">
+           
+            <header class="bg-blue-900 py-4 text-center w-full">
+                <h1 class="text-yellow-400 text-2xl font-bold uppercase">
+                    Selamat Datang di Website SD Negeri Padangsari 01
+                </h1>
+            </header>
+
+           
+            <div class="max-w-5xl mx-auto mt-10 p-5 bg-gray-100 shadow-md rounded-lg flex flex-col md:flex-row items-center md:items-start gap-10">
+                <div class="flex flex-col items-center">
+                    <h2 class="text-lg font-bold mb-5">Sambutan Kepala Sekolah</h2>
+                    <img
+                        src="/images/kepala-sekolah.jpg" 
+                        alt="Kepala Sekolah" 
+                        class="w-40 h-50 object-cover rounded-md shadow-md mb-4"
+                    />
+                </div>
+                <div class="text-gray-700 text-justify max-w-lg">
+                    <p>Assalamu'alaikum wr.wb.</p>
+                    <p class="mt-2">Puji syukur kami panjatkan kehadirat  Allah SWT, Tuhan Yang Maha Esa yang telah memberikan rahmat dan hidayahNya sehingga pembuatan website SD Negeri Padangsari 01 Semarang ini dapat terlaksana dengan lancar tanpa suatu halangan apa pun. kami merasa bangga mendapatkan kesempatan untuk mengikuti workshop pelatihan pembuatan website sekolah. Kami akan berupaya untuk mengembangkan ilmu yang sudah diberikan melalui workshop untuk kemajuan SD Negeri Padangsari 01 terutama dibidang pendidikan dan memeberikan informasi secara detail tentang SD Negeri Padangsari 01. Dilihat dari perkembangan zaman, teknologi dan kebutuhan akan informasi mau tidak mau kita harus mengikutinya.</p>
+                    <p class="mt-2">Kami berusaha menyajikan informasi tentang Siswa, Guru, karyawan, tendik dan kegiatan-kegiatan disekolah SDN Padangsari 01, informasi atau pengumuman penting yang dibutuhkan oleh masyarakat umum. selain itu, kami juga memberikan sedikit informasi tentang tempat Pariwisata, Kesehatan yang ada disekitar SDN Padangsari 01. </p>
+                    <p class="mt-2">Semoga dengan adanya website ini dapat membantu dunia pariwisata, pendidikan dan masyarakat umum untuk mengetahui dan memahami SDN Padangsari 01 dan sekitarnya. Kami berharap, dengan adanya website ini dapat memberikan manfaat bagi semua pihak yang membutuhkan. Besar harapan kami mengharapkan masukan dari berbagai pihak agar website kami lebih bagus dalam segi tampilan dan lain-lain sehingga dapat memenuhi kebutuhan akan informasi dalam dunia pendidikan khususnya. Kami akan terus belajar, menggembangkan dan memperbaiki dalam segi tampilan, isi dan mutu website. Terimakasih  atas dukungannya, semoga website kami lebih maju untuk mencapai SD Negeri Padangsari 01 yang lebih baik..</p>
+                    <p class="mt-2">Wassalamu'alaikum wr.wb.</p>
+                </div>
+            </div>
+        </div>
