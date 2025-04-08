a54689355d33145926ab28f904e015a41f1c326a
rezaaditya16 <rezaramadhanaditya764@gmail.com>
2025-04-08 23:11:43 +0700

    Add Classroom, Student, Teacher, and Gallerie models with migrations; fix Post class name casing

---
+++ app/Models/Classroom.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Classroom extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+
+    public function teachers()
+    {
+        return $this->hasMany(Teacher::class);
+    }
+}
