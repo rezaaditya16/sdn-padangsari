e47b9350db96311a70ab76d7c42a15350f40cee9
rezaaditya16 <rezaramadhanaditya764@gmail.com>
2025-04-14 00:29:34 +0700

    Enhance StudentResource: add form fields for name, classroom, and photo; update navigation icon and group; implement view page for students; create view page for posts

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -7,24 +7,47 @@ use App\Filament\Resources\StudentResource\RelationManagers;
 use App\Models\Student;
 use Filament\Forms;
 use Filament\Forms\Form;
+use App\Models\Classroom;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class StudentResource extends Resource
 {
-    protected static ?string $navigationGroup = 'Data Sekolah';
     protected static ?string $model = Student::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+    protected static ?string $navigationGroup = 'Data Sekolah';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Select::make('classroom_id')
+                        ->label('Classroom')
+                        ->options(Classroom::all()->pluck('name', 'id'))
+                        ->searchable()
+                        ->required()
+                        ->reactive()
+                        ->afterStateUpdated(function ($state, callable $set) {
+                            $classroom = Classroom::find($state);
+                            $set('class', $classroom ? $classroom->name : '');
+                        }),
+                TextInput::make('class')
+                    ->required()
+                    ->maxLength(255),
+                FileUpload::make('photo')
+                    ->image()
+                    ->nullable(),
             ]);
     }
 
@@ -32,18 +55,23 @@ class StudentResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('class')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime(),
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
@@ -60,6 +88,7 @@ class StudentResource extends Resource
             'index' => Pages\ListStudents::route('/'),
             'create' => Pages\CreateStudent::route('/create'),
             'edit' => Pages\EditStudent::route('/{record}/edit'),
+            'view' => Pages\ViewStudent::route('/{record}'),
         ];
     }
 }
