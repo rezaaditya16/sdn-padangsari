0f4248fe70a288656f89c3075a86f857e7a04826
rezaaditya16 <rezaramadhanaditya764@gmail.com>
2025-04-14 08:39:40 +0700

    Implement Gallery resource: add model, resource pages, and seeder with initial gallery data

---
+++ app/Filament/Resources/GalleryResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GalleryResource\Pages;
+use App\Models\Gallery;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+
+class GalleryResource extends Resource
+{
+    protected static ?string $model = Gallery::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-photo';
+
+    public static function form(Forms\Form $form): Forms\Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('description')
+                    ->maxLength(500),
+                FileUpload::make('images')
+                    ->label('Gallery Images')
+                    ->multiple()
+                    ->image()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Tables\Table $table): Tables\Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('description')
+                    ->limit(50),
+                TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGalleries::route('/'),
+            'create' => Pages\CreateGallery::route('/create'),
+            'edit' => Pages\EditGallery::route('/{record}/edit'),
+        ];
+    }
+}
